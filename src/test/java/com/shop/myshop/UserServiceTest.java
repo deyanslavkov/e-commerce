7734f6407e70d5628fb0e1411b0335acb81c72fa
deyanslavkov <deyanslavkov@gmail.com>
2025-05-18 12:48:03 +0300

    Test failing build preventing merge into main

--- src/test/java/com/shop/myshop/UserServiceTest.java
+++ src/test/java/com/shop/myshop/UserServiceTest.java
@@ -2,10 +2,13 @@ package com.shop.myshop;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class UserServiceTest {
 
     @Test
     public void test() {
-
+        assertTrue(false);
     }
 }
